a45f5ab0e9efc9ca1fa267842b78f4e6c9bf1174
LUCY <wangecikariu.luc@gmail.com>
2023-07-10 22:13:51 +0300

    Arithmetic Lab

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
 
 // Write your code here
+let num1 = 31;
+let num2 = 2;
+let multiply = 62
+
+let Math.floor(Math.random() * 10) + 2;
+
+let num3 = 22;
+let num4 = 6;
+let mod = 4
+
+let Math.max(5, 10, 15, 20);
+
+
