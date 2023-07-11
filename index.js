cf3110423d56302c3859ee400786308a0180d8e8
LUCY <wangecikariu.luc@gmail.com>
2023-07-11 20:21:13 +0300

    Corrcted Arithmetics Lab

--- index.js
+++ index.js
@@ -2,14 +2,13 @@
 // Write your code here
 let num1 = 31;
 let num2 = 2;
-let multiply = 62
+let multiply = num1 * num2;
 
-let Math.floor(Math.random() * 10) + 2;
+let random = Math.floor(Math.random() * 10) + 2;
 
 let num3 = 22;
 let num4 = 6;
-let mod = 4
+let mod = num3 % num4;
 
-let Math.max(5, 10, 15, 20);
-
+let max = Math.max(5, 10, 15, 20)
 
